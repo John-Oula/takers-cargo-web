cf3a6b6bc6beda6e6a717552baa73763f9bf6b2a
John-Oula <johnoula@icloud.com>
2022-04-16 11:39:02 +0800

    Update index.js

--- pages/book/index.js
+++ pages/book/index.js
@@ -405,7 +405,7 @@ unit:
                                 </Select>
                             <InputGroup alignItems={`center`}>
                                 <Input  {...register('unit')} mt={5} placeholder={transportation == `air` ? 'Weight/pcs' : `cubic metres`} type={`number`} />
-                                <InputRightAddon  ></InputRightAddon>
+                                <InputRightAddon  >{bailmentSelectValue ? bailmentSelectValue.unit : `--`}</InputRightAddon>
                                 </InputGroup>
                                 {/* <Input mt={5}  placeholder='Value (USD)' type={`number`} /> */}
                                 {bailmentSelectValue?.unit != 'pcs'  ? <Input  {...register('quantity')} mt={5} placeholder='Quantity' type={`number`} /> : <></>}
