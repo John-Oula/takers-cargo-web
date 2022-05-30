ba868ef30e1e57b9afc30303300b634d3870c6c0
John-Oula <johnoula@icloud.com>
2022-05-30 12:42:15 +0800

    Update Services.js

--- Sections/Services/Services.js
+++ Sections/Services/Services.js
@@ -12,13 +12,31 @@ import {
   import { CheckIcon } from '@chakra-ui/icons';
   
   // Replace test data with your own
-  const features = Array.apply(null, Array(8)).map(function (x, i) {
-    return {
-      id: i,
-      title: 'Lorem ipsum dolor sit amet',
-      text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
-    };
-  });
+  const features = 
+     [
+      {
+     id: 1,
+     title: 'Costless Services',
+     text: 'We carriers in all modes of transportation and work to establish a sound business relationship with them based on our qualification process and their ability to provide a reliable and quality service. Our satisfied customers are realizing significant bottom-line freight savings while experiencing a reliable and consistent service.',
+   },
+   {
+  id: 2,
+  title: 'Customer Management',
+  text: 'We recognize that each customer may be different with unique requirements so we provide our customer with the personalized service touch. A dedicated Customer Service Representative, (CSR) is assigned. We ensure costumer satisfactions.',
+},
+{
+id: 3,
+title: 'In depth industry knowledge',
+text: 'Our team of professionals understands the importance of customers’ feedback, challenges, and game changing events so we can assess the impact to our customers. Based on our assessment we will determine go forward steps if at all necessary to ensure our customers are looked after accordingly.',
+},
+{
+id: 4,
+title: 'Multiple value add services',
+text: 'We offer our customers many different value add services which have been designed to introduce bottom line savings and enhance our customer support capabilities to our valued customers.',
+},
+
+  ]
+
   
   export default function AppFeatures() {
     return (
@@ -26,9 +44,8 @@ import {
         <Stack mb={10} spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
           <Heading fontSize={'3xl'}>Why choose us?</Heading>
           <Text color={'gray.600'} fontSize={'xl'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua.
+          We are a team of logistics management team who you can trust and rely on to effectively manage your transportation service requirements. With good management experience, we provide our customers with cost effective transportation solutions. Our commitment to investment in state of the art business intelligence, visibility and tracking technology and taking pride in being a total solutions service provider is what sets us apart from your typical transportation company.
+           Takers Cargo air freight forwarding process of planning and organizing the transport of freight from one point to another by air gives businesses the flexibility and trusted experience they need in a transportation partner.
           </Text>
         </Stack>
   
