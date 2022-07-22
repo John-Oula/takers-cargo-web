e61a306da269d47633a6e10b7a5892f58f1438bd
John-Oula <johnoula@icloud.com>
2022-07-23 00:25:36 +0800

    loading icon

--- Components/Loading.js
+++ Components/Loading.js
@@ -1,12 +1,13 @@
 import { Flex, Spinner, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import React from 'react'
-import logo from '../assets/takers_white_bg.jpg'
+import logo from '../assets/logo.svg'
 
 export default function Loading() {
   return (
-    <Flex flexDirection={`column`} alignItems={`center`} justifyContent={`center`} height={`100%`} width={`100%`}>
-        <Image src={logo} width={`600pt`} height={`600pt`}/>
+    <Flex flexDirection={`column`} alignItems={`center`} justifyContent={`center`} bgColor={`#000`} height={`100vh`} width={`100%`}>
+        <Image src={logo} width={`60pt`} height={`60pt`}/>
+        <br/>
         <Spinner  />
         <Text textAlign={`center`} mt={5} color={`#ed8b00`}>Turn on VPN if you are based in China</Text>
 
